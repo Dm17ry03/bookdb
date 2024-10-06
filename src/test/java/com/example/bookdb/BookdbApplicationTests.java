6198f17578c50013817391344ebd32d55d5b25fd
dmitrykh <dmitry.norge@gmail.com>
2024-10-06 18:24:10 +0200

    created basic autotests for the db

--- src/test/java/com/example/bookdb/BookdbApplicationTests.java
+++ src/test/java/com/example/bookdb/BookdbApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.bookdb;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookdbApplicationTests {
+
+	@Container
+	public static PostgreSQLContainer<?> postgresContainer =
+			new PostgreSQLContainer<>("postgres:17")
+					.withDatabaseName("testdb")
+					.withUsername("test")
+					.withPassword("test");
+					//.withExposedPorts(5433);
+
+	@DynamicPropertySource
+	static void registerPgProperties(DynamicPropertyRegistry registry) {
+		// Configure Spring Boot's datasource properties dynamically based on container's details
+		registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresContainer::getUsername);
+		registry.add("spring.datasource.password", postgresContainer::getPassword);
+	}
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
